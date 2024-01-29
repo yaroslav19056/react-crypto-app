6a39f32f052ad36bfa822090cd423b5cff29d1ab
Yaroslav <mudryjaroslav146@gmail.com>
2024-01-29 18:39:15 +0300

    init app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { CryptoContextProvider } from './context/crypto-context';
+import AppLayout from './components/layout/AppLayout';
+
+
+
+export default function App() {
+  return (
+  <CryptoContextProvider>
+    <AppLayout />
+  </CryptoContextProvider>
+)
+}
